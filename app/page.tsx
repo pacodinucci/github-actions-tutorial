b43ff9658a34ae565ea296c72547a193f3bcd1be
pacodinucci <franciscoldinucci@gmail.com>
2025-02-10 15:42:45 -0300

    hello world

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>GITHUB ACTIONS TUTORIAL</h1>
+      <h2>Hello world</h2>
     </main>
   );
 }
