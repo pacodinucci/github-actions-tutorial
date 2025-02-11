38c254b0c750a7508558275e5c8c8c641d335ca7
pacodinucci <franciscoldinucci@gmail.com>
2025-02-11 09:45:51 -0300

    workflow node version

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>GITHUB ACTIONS TUTORIAL</h1>
       <h2>Hello world With PM2!!</h2>
+      <h3>Node Version 20.x</h3>
     </main>
   );
 }
