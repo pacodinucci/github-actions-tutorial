4df0ff23d7f2f5f6cfc8756f5534cbdffea002e7
pacodinucci <franciscoldinucci@gmail.com>
2025-02-11 10:46:05 -0300

    workflow node fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-bold text-red-500">GITHUB ACTIONS TUTORIAL</h1>
+      <h1 className="font-bold text-green-600">GITHUB ACTIONS TUTORIAL</h1>
       <h2>Hello world With PM2!!</h2>
       <h3>Node Version 20.x</h3>
     </main>
